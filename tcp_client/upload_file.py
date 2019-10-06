ab99d946fc0555e8bdcc1f8a6136255601610ba3
Emiliano Prediger <emprediger@teco.com.ar>
2019-10-06 14:07:51 -0300

    TCP upload file

--- tcp_client/upload_file.py
+++ tcp_client/upload_file.py
@@ -1,13 +1,59 @@
-import socket
+import socket, os
+
+CHUNK_SIZE = 1024
 
 def upload_file(server_address, src, name):
   # TODO: Implementar TCP upload_file client
   print('TCP: upload_file({}, {}, {})'.format(server_address, src, name))
 
-  # Se crea socket TCP
+  # Creación socket TCP
   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
   # Conexión contra svr-host:svr-port
   sock.connect(server_address)
 
+  # Envío largo nombre de archivo
+  filename_size = len(name)
+  bytes_sent = 0
+  while bytes_sent < len(str(filename_size)):
+    bytes_sent += sock.send(str(filename_size).encode())
+
+  # Aguardo OK de svr
+  signal = sock.recv(CHUNK_SIZE).decode()
+
+  if signal != 'start':
+    print('Error receiving name size from server')
+    return exit(1)
+
+  # Envío nombre de archivo al servidor
+  bytes_sent = 0
+  while bytes_sent < filename_size:
+    bytes_sent += sock.send(name.encode())
+
+  # Envío archivo al servidor
+  f = open(src, "rb")
+  f.seek(0, os.SEEK_END)
+  file_size = f.tell()
+  f.seek(0, os.SEEK_SET)
+
+  print("Sending {} bytes from {}".format(file_size, src))
+
+  # Envío tamaño de archivo en bytes
+  sock.send(str(file_size).encode())
+  signal = sock.recv(CHUNK_SIZE)
+
+  if signal.decode() != "start":
+    print("There was an error on the server")
+    return exit(1)
+
+  bytes_sent = 0
+  while bytes_sent < file_size:
+    chunk = f.read(CHUNK_SIZE)
+    if not chunk:
+      break
+    bytes_sent += sock.send(chunk)
+
+  # Cierro archivo
+  f.close()
+  # Cierro socket
   sock.close()
--- tcp_server/start_server.py
+++ tcp_server/start_server.py
@@ -1,5 +1,7 @@
 import socket
 
+CHUNK_SIZE = 1024
+
 def start_server(server_address, storage_dir):
   # DOING: Implementar TCP server
   # host = server_address[0]
@@ -22,4 +24,42 @@ def start_server(server_address, storage_dir):
 
     print("Accepted connection from {}".format(addr_info))
 
+    # Recibo file_name length
+    filename = ''
+
+    # TODO: ver validar cuando recibi todo el numero
+    filename_size = int(conn.recv(CHUNK_SIZE).decode())
+    print('filename length: {}'.format(filename_size))
+
+    # Envío a svr OK TODO: revisar
+    conn.send(b'start')
+
+    # Recepción nombre de archivo
+    bytes_recv = 0
+    while bytes_recv < filename_size:
+      data = conn.recv(CHUNK_SIZE).decode()
+      filename += data
+      bytes_recv += len(data)
+
+    filename = "{}/{}".format(storage_dir,filename)
+    print('filename: {}'.format(filename))
+
+    f = open(filename, "wb")
+    #
+    file_size = int(conn.recv(CHUNK_SIZE).decode())
+    conn.send(b'start')
+
+    bytes_recv = 0
+    while bytes_recv < file_size:
+      data = conn.recv(CHUNK_SIZE)
+      bytes_recv += len(data)
+      f.write(data)
+
+    f.close()
+    print("Received file {}".format(filename))
+
   sock.close()
+
+def recv_cmd():
+  """ Se recibe por parte del cliente si quiere subir o descargar un archivo """
+  pass
