73a6ad7acac58636db970578468ed5be76bd17b3
Emiliano Prediger <emprediger@teco.com.ar>
2019-10-08 18:44:40 -0300

    comment funciones y correciones

--- tcp_server/start_server.py
+++ tcp_server/start_server.py
@@ -3,36 +3,38 @@ import socket, os
 CHUNK_SIZE = 1024
 
 def create_dir(storage_dir):
+  """ Validación de directorio, si no existe lo crea. """
   if not os.path.exists(storage_dir):
     os.mkdir(storage_dir)
-    print('Dir: {} created!'.format(storage_dir)) 
+    print('Dir: {} created!'.format(storage_dir))
 
-def recieve(conn,size):
+def receive(conn, size):
+  """ Servidor recibe mensaje de tamaño size """
   full_data = ''
+
   bytes_recv = 0
   while bytes_recv < size:
     data = conn.recv(CHUNK_SIZE).decode()
     full_data += data
     bytes_recv += len(data)
+
   return full_data
 
 def start_server(server_address, storage_dir):
   """ DOING: Implementación TCP server
+
   Backlog:
     * modularizar
-    * clase socket? 
+    * clase socket?
   """
-  # host = server_address[0]
-  # port = server_address[1]
-
   create_dir(storage_dir)
 
   print('TCP: start_server({}, {})'.format(server_address, storage_dir))
 
-  # Se crea socket TCP
+  # Creación socket TCP
   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-  # Se bindea socket a host:port
+  # Bind socket a host:port
   sock.bind(server_address)
 
   # Se prepara para aceptar conexiones - TODO: por ahora 1 cliente
@@ -45,7 +47,7 @@ def start_server(server_address, storage_dir):
 
     print("Accepted connection from {}".format(addr_info))
 
-    # Recibo file_name length
+    # Recepción file_name length
     # TODO: ver validar cuando recibi todo el numero
     filename_size = int(conn.recv(CHUNK_SIZE).decode())
     print('filename length: {}'.format(filename_size))
@@ -54,14 +56,14 @@ def start_server(server_address, storage_dir):
     conn.send(b'start')
 
     # Recepción nombre de archivo
-    filename = recieve(conn,filename_size)
-    filename = "{}/{}".format(storage_dir,filename)
+    filename = receive(conn, filename_size)
+    filename = "{}/{}".format(storage_dir, filename)
     print('filename: {}'.format(filename))
 
     # Recibo comando (download o upload)
     cmd_length = int(conn.recv(CHUNK_SIZE).decode())
     conn.send(b'start')
-    command = recieve(conn,cmd_length)
+    command = receive(conn, cmd_length)
 
     if command == 'download':
       # Envío de archivo al cliente
