36e112b54a797edcec47d5cba358daf8c748f521
jazmin.ferreiro <jazmin.ferreiro@despegar.com>
2019-10-11 16:26:14 -0300

    big file tested

--- tcp/Connection.py
+++ tcp/Connection.py
@@ -11,27 +11,29 @@ from tcp.TcpSocket import TcpSocket
 """
 
 
-class ConnectionProtocol(object):
+class Connection:
     CHUNK_SIZE = 1024
+    NUM_LEN = 10
     CODE_LEN = 2
+
     OK = "ok"
     DOWNLOAD = "dl"
     UPLOAD = "ul"
 
-    def __innit__(self, storage_dir: str, conn_socket: TcpSocket):
-        self.storage_dir = storage_dir
-        self.conn_socket = conn_socket
+    def __init__(self, socket: TcpSocket):
+        self.socket = socket
+        if not (self.socket.is_connected):
+            raise RuntimeError("Connection is broken")
 
-    # envio y recibo codigos de 2 bytes
-
+    # envio y recibo codigos de 2 bytes para los comandos
     def send_code(self, code: str):
-        self.conn_socket.send_bytes(code.encode())
+        self.socket.send_bytes(code.encode())
 
     def recv_code(self)-> str:
-        return self.conn_socket.receive_bytes(self.CODE_LEN).decode()
+        return self.socket.receive_bytes(self.CODE_LEN).decode()
 
     def wait_ok_signal(self):
-        signal = self.recv_code().decode()
+        signal = self.recv_code()
         if signal != self.OK:
             print("There was an error in connection, ok never received")
             return exit(1)
@@ -39,15 +41,16 @@ class ConnectionProtocol(object):
     # envio y recibo numeros
 
     def send_number(self, num: int):
-        self.conn_socket.send_bytes(str(int).encode())
+        nun_in_ten_bytes = '{:>10}'.format(str(num)).encode()
+        self.socket.send_bytes(nun_in_ten_bytes)
 
     def recv_number(self) -> int:
-        data = self.conn_socket.receive_bytes(self.CHUNK_SIZE)
+        data = self.socket.receive_bytes(self.NUM_LEN)
         return int(data)
 
     # envio y recibo nombres de archivos
 
-    def recv_filename(self):
+    def recv_filename(self)-> str:
         # Recepción file_name length
         filename_size = self.recv_number()
         print('filename length: {}'.format(filename_size))
@@ -56,12 +59,10 @@ class ConnectionProtocol(object):
         self.send_code(self.OK)
 
         # Recepción nombre de archivo
-        filename = self.conn_socket.receive_bytes(filename_size)
-        filename = "{}/{}".format(self.storage_dir, filename)
-        print('filename: {}'.format(filename))
-        return filename
+        filename = self.socket.receive_bytes(filename_size)
+        return filename.decode()
 
-    def send_filename(self, name):
+    def send_filename(self, name: str):
         # Envío largo nombre de archivo
         name = name.encode()
         filename_size = len(name)
@@ -71,33 +72,33 @@ class ConnectionProtocol(object):
         self.wait_ok_signal()
 
         # Envío nombre de archivo al servidor
-        self.conn_socket.send_bytes(name)
+        self.socket.send_bytes(name)
 
     # envio y recibo archivos
 
-    def recv_file(self, filename):
+    def recv_file(self, filename_path):
         # Recepción cantidad de bytes de archivo
         file_size = self.recv_number()
         self.send_code(self.OK)
 
-        f = open(filename, "wb")
+        f = open(filename_path, "wb")
 
         bytes_recv = 0
         while bytes_recv < file_size:
-            data = self.conn_socket.receive_bytes(self.CHUNK_SIZE)
+            data = self.socket.receive_bytes(min(self.CHUNK_SIZE, file_size - bytes_recv))
             bytes_recv += len(data)
             f.write(data)
         f.close()
-        print("Received file {}".format(filename))
+        print("Received file {}".format(filename_path))
 
-    def send_file(self, filename):
+    def send_file(self, filename_path):
         # Envío de archivo
-        f = open(filename, "rb")
+        f = open(filename_path, "rb")
         f.seek(0, os.SEEK_END)
         file_size = f.tell()
         f.seek(0, os.SEEK_SET)
 
-        print("Sending {} bytes from {}".format(file_size, filename))
+        print("Sending {} bytes from {}".format(file_size, filename_path))
 
         # Envío tamaño de archivo en bytes
         self.send_number(file_size)
@@ -105,10 +106,10 @@ class ConnectionProtocol(object):
 
         bytes_sent = 0
         while bytes_sent < file_size:
-            chunk = f.read(self.CHUNK_SIZE)
+            chunk = f.read(min(self.CHUNK_SIZE, file_size - bytes_sent))
             if not chunk:
                 break
-            bytes_sent += self.conn_socket.send_bytes(chunk)
+            bytes_sent += self.socket.send_bytes(chunk)
 
         # Cierro archivo
         f.close()
--- tcp/TcpSocket.py
+++ tcp/TcpSocket.py
@@ -1,37 +1,37 @@
 import socket
 
 CHUNK_SIZE = 1024
+NUM_SIZE = 10
 
 """
 Socket TCP
 envia y recibe bytes
 """
-class TCP_Socket:
-  def __init__(self, server_address):
+class TcpSocket:
+  def __init__(self, server_address: tuple, is_connected: bool = False, conn_socket = None):
       # Creación socket TCP
-      self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       self.server_address = server_address
-      self.is_connected = False
+      self.is_connected = is_connected
+      if(self.is_connected and conn_socket != None):
+          self.sock = conn_socket
+      else:
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-  def _init(self, conn_socket):
-      self.sock = conn_socket
-      self.is_connected = True
 
-
-  def bind_and_listen(self):
+  def bind_and_listen(self) :
       # Bind socket a host:port
       self.sock.bind(self.server_address)
       # Se prepara para aceptar conexiones - TODO: por ahora 1 cliente
       self.sock.listen(1)
 
-  def accept(self) -> 'TCP_Socket':
+  def accept(self) -> 'TcpSocket':
       conn_sock, addr_info = self.sock.accept()
       if not conn_sock:
           print("Not connected")
           raise RuntimeError("socket error accepting connection")
       self.is_connected = True
       print("Accepted connection from {}".format(addr_info))
-      return TCP_Socket(conn_sock)
+      return TcpSocket(self.server_address, self.is_connected,conn_sock)
 
   def connect(self) :
       self.sock.connect(self.server_address)
@@ -43,7 +43,9 @@ class TCP_Socket:
       self.is_connected = False
 
 
-  def send_bytes(self, msg: bytes):
+  def send_bytes(self, msg: bytes)-> int:
+      if not self.is_connected:
+          raise RuntimeError("socket not connected")
       msg_len = len(msg)
       total_sent = 0
       while total_sent < msg_len:
@@ -52,9 +54,12 @@ class TCP_Socket:
               print("Error sending msg")
               raise RuntimeError("socket connection broken")
           total_sent = total_sent + sent
+      return total_sent
 
 
   def receive_bytes(self, size: int)-> bytes:
+      if not self.is_connected:
+          raise RuntimeError("socket not connected")
       chunks = []
       bytes_recd = 0
       while bytes_recd < size:
